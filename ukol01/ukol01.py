2ddbd2978890b6566e06d86734f3f365f9e520dd
Bohdan <protzzz.it@gmail.com>
2023-02-21 20:17:55 +0100

    updated ukol01.py file. right one.

--- ukol01/ukol01.py
+++ ukol01/ukol01.py
@@ -1,32 +1,38 @@
-kniha = []
-stav = False
+knihovna = []
 
-def pridej_knihu(nazev, autor_jmeno, autor_prijmeni):
-    kniha = {"nazev":nazev, "autor_jmeno":autor_jmeno, "autor_prijmeni":autor_prijmeni}
-    print(kniha)
+def pridej_knihu (nazev, autor_jmeno, autor_prijmeni):
+    kniha = {"nazev": nazev, "autor": f"{autor_jmeno} {autor_prijmeni}", "vypujcena": False}
+    knihovna.append(kniha)
 
 def vypujceni(nazev):
-    for i in kniha:
-        if i["nazev"] == nazev:
-            stav = True
+    for kniha in knihovna:
+        if kniha["nazev"] == nazev:
+            kniha["vypujcena"] == True
+            break
 
 def vraceni(nazev):
-    for i in kniha:
-        if i["nazev"] == nazev:
-            stav = False
+    for kniha in knihovna:
+        if kniha["nazev"] == nazev:
+            kniha["vypujcena"] == False
+            break
 
-def vypujceni_knihy():
-    for i in kniha:
-        if stav == True:
-            print(kniha[i])
+def vypujcene_knihy():
+    vypujcene_knihy = []
+    for kniha in knihovna:
+        if kniha["vypujcena"]:
+            vypujcene_knihy.append(kniha)
+    return vypujcene_knihy
+
+
+vk = vypujcene_knihy()
+print("Seznam vypůjčených knih:")
+for k in vk:
+    print(k["nazev"])
 
 pridej_knihu("Algoritmy v C","Robert","Sedgewick")
-pridej_knihu("TheArt ofComputerProgramming, Volume1","Donald","Knuth")
-pridej_knihu("TheArt ofComputerProgramming, Volume2","Donald","Knuth")
+pridej_knihu("The Art of Computer Programming, Volume 1","Donald","Knuth")
+pridej_knihu("The Art of Computer Programming, Volume 2","Donald","Knuth")
+vypujceni("The Art of Computer Programming, Volume 1")
+vypujceni("The Art of Computer Programming, Volume 2")
 
-vypujceni("TheArt ofComputerProgramming, Volume1")
-vypujceni("TheArt ofComputerProgramming, Volume2")
-
-
-print("Seznam vypůjčených knih:")
-vypujceni_knihy()
+    

df7560a8d49762752b71080d6e74ad74be88931a
Bohdan <protzzz.it@gmail.com>
2023-03-02 10:19:55 +0100

    added ukol03.py and pdf file

---
+++ lekce03/ukol03.py
@@ -0,0 +1,28 @@
+def lenght(array):
+    return len(array)
+
+def init_queue(n):
+    k=[None for x in range(n)]
+    s={"data":k, "head":0, "tail":0}
+    return s
+
+def empty(q):
+    return q["head"] == q["tail"]
+
+def enqueue(x):
+
+    return
+
+def dequeue():
+
+    return
+
+
+print(init_queue(10))
+# for i in range(10):
+#     enqueue(i)
+# for i in range(10):
+#     print(dequeue(),end=",")
+
+
+    
